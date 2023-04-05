51ba5fa1b802206cf13e19535606b051c2296420
jfoyarzo <foyarzo621@gmail.com>
2023-04-04 22:51:01 -0400

    Add content to hero and update sections id

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,14 +13,14 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
-      <Page useSplashScreenAnimation>
+      <Seo title="Felipe Oyarzo's Portfolio" />
+      <Page>
         <HeroSection sectionId="hero" />
         <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <AboutSection sectionId="about" heading="About Me" />
+        <InterestsSection sectionId="stack" heading="Tech Stack" />
+        <ProjectsSection sectionId="projects" heading="Projects" />
+        <ContactSection sectionId="contact" heading="Contact Me" />
       </Page>
     </>
   );

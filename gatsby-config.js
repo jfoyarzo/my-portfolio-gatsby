01ce9537493495de9f1e6dc1c80d92c0fd9bc9e8
jfoyarzo <foyarzo621@gmail.com>
2023-04-04 22:50:38 -0400

    Add basic site content and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,18 +6,18 @@ module.exports = {
         siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
-          shortName: "Portfolio", // Used in manifest.json
+          siteName: "Felipe Oyarzo's Portfolio", // Used in manifest.json
+          shortName: "F.O. - Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
           display: "minimal-ui", // Used in manifest.json
         },
         contentDirectory: "./content",
-        blogSettings: {
-          path: "/blog", // Defines the slug for the blog listing page
-          usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
-        },
+        //blogSettings: {
+          //path: "/blog", // Defines the slug for the blog listing page
+          //usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+        //},
         // googleAnalytics: {
         //     trackingId: "UA-XXXXXX-X",
         //     anonymize: true, // Default true

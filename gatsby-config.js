db044f7ec03d7f710cb0467e20b4a99bafe7457f
jfoyarzo <foyarzo621@gmail.com>
2023-04-17 14:29:23 -0400

    Remove code comments from gatsby.config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,26 +3,20 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/",
         manifestSettings: {
-          favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "Felipe Oyarzo's Portfolio", // Used in manifest.json
-          shortName: "F.O. - Portfolio", // Used in manifest.json
-          startUrl: "/", // Used in manifest.json
-          backgroundColor: "#FFFFFF", // Used in manifest.json
-          themeColor: "#000000", // Used in manifest.json
-          display: "minimal-ui", // Used in manifest.json
+          favicon: "./content/images/favicon.png",
+          siteName: "Felipe Oyarzo's Portfolio",
+          shortName: "F.O. - Portfolio",
+          startUrl: "/",
+          backgroundColor: "#FFFFFF",
+          themeColor: "#000000",
+          display: "minimal-ui",
         },
         contentDirectory: "./content",
         blogSettings: {
-          path: "__disabled", // Defines the slug for the blog listing page
-          //usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+          path: "__disabled",
         },
-        // googleAnalytics: {
-        //     trackingId: "UA-XXXXXX-X",
-        //     anonymize: true, // Default true
-        //     environments: ["production", "development"] // Default ["production"]
-        // }
       },
     },
   ],
